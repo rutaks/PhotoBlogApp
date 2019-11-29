0fa9a637261b57e17a38d1299c2b3f592a39fbae
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-29 05:50:14 +0200

    Added Missing Comments

--- app/src/main/java/com/example/photoblogapp/LoginActivity.java
+++ app/src/main/java/com/example/photoblogapp/LoginActivity.java
@@ -3,12 +3,32 @@ package com.example.photoblogapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button buttonLogin;
+    private Button buttonSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setupForm();
+    }
+
+    /**
+     * Binds XML Layout elements To Code Variables
+     * @return      void
+     */
+    private void setupForm(){
+        editTextEmail = findViewById(R.id.edit_text_login_email);
+        editTextPassword = findViewById(R.id.edit_text_login_password);
+        buttonLogin = findViewById(R.id.button_login);
+        buttonSignup = findViewById(R.id.button_register);
     }
 }
