1c3a82f8a0aa38ca959e8b7b79491b0b451fea62
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-29 05:44:18 +0200

    Added Check if user logged in functionality

--- app/src/main/java/com/example/photoblogapp/MainActivity.java
+++ app/src/main/java/com/example/photoblogapp/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,8 +19,22 @@ public class MainActivity extends AppCompatActivity {
         goToLogin();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!isLoggedIn()){
+            goToLogin();
+        }
+    }
+
     public void goToLogin(){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public Boolean isLoggedIn(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        return firebaseUser != null ? true : false;
     }
 }
